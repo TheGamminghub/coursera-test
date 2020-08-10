7ec4ff51d90b28aa80e98db61ab89e84839b8ed5
TheGaminghub <TheGaminghub@example.com>
2020-08-10 14:21:52 +0530

    lecture 06 updated

--- Lecture06/app.js
+++ Lecture06/app.js
@@ -1,12 +1,24 @@
 (function () {
 'use strict';
 
-  angular.module('myFirstApp', [])
-  .controller('myFirstController',function($scope){
-    $scope.name="TheGaminghub";
-    $scope.sayHello=function functionName() {
-      return "Hello TheGaminghub";
+  angular.module('NameCalculator', [])
+  .controller('NameCalculatorController',function($scope){
+    $scope.name="";
+    $scope.totalvalue=0;
+
+    $scope.displayNumeric = function() {
+      var totalNamevalue = calculatNumericForString($scope.name);
+
+      $scope.totalvalue=totalNamevalue;
+    };
+    function calculatNumericForString(string) {
+      var totalStringValue=0;
+      for(var i=0; i < string.length; i++){
+        totalStringValue=string.charCodeAt(i)+totalStringValue;
+      }
+      return totalStringValue;
     }
   });
+
 
 })();
